61625e7092def6b8972295acc2d151e0a485a240
Burak Atilla <burak.atilla@gmail.com>
2022-01-16 18:52:13 +0300

    5 | html css and js done. its working. still needs a final check

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const closeBtn = document.querySelectorAll(".close");
 const addBtn = document.querySelector("#buton1");
 const liste = document.querySelector("#list");
+const taskInput = document.querySelector("#input");
 
 //close button function
 liste.addEventListener("click", (e) => {
@@ -8,3 +9,16 @@ liste.addEventListener("click", (e) => {
     e.target.parentElement.style.display = "none";
   }
 });
+
+function addFunc() {
+  const newLi = document.createElement("li");
+  newLi.innerHTML = `<li id="itemLi">${taskInput.value}<span class="close"></span></li>`;
+  liste.appendChild(newLi);
+}
+
+function liToggle(e) {
+  e.target.classList.toggle("checked");
+}
+
+liste.addEventListener("click", liToggle);
+addBtn.addEventListener("click", addFunc);
